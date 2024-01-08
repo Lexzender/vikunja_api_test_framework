d8bca30c3512b745e0f53fa351857db86163182a
Aleksei Kostromin <kostrominpav@gmail.com>
2024-01-08 15:51:02 +0400

    update

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,7 @@ import allure
 from tests.conftest import API_request
 from tests.test_create_user import created_user
 
+
 @allure.title('Successful authorization')
 @allure.feature('Authorization with API')
 @allure.label('layer', 'API')
@@ -11,11 +12,13 @@ from tests.test_create_user import created_user
 @allure.severity('critical')
 def test_login_user_succsessfuly():
     data = created_user
-
     with allure.step("Отправляем запрос на получение jwt токена"):
         results = API_request("login", "post", json=data)
 
-    with allure.step("Сохраняем jwt токен"):
+    with allure.step("Проверяем, что в ответе статус код == 200"):
+        assert results.status_code == 200
+
+    with allure.step("Сохраняем jwt токен из ответа"):
         jwt_token = results.json()["token"]
 
         return jwt_token
